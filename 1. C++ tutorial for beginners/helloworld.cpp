6bb82cfed202a98bb266df8418fc52332fbb8e5b
minhtuan1407 <m.tuan9a2@gmail.com>
2023-11-15 08:49:46 +0700

    change: update code session 1

---
+++ 1. C++ tutorial for beginners/helloworld.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+
+    // This is a comment
+    /* 
+        This is a multi-line comment
+    */
+
+    std::cout << "Hello World!" << std::endl;
+    std::cout << "I'm a C++ program" << std::endl;
+
+    return 0;
+}
