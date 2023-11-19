7f80ee257b6e726d7c02cd80fedaf3c8af08dbf9
minhtuan1407 <m.tuan9a2@gmail.com>
2023-11-19 20:08:52 +0700

    change: update code session 6

---
+++ 6. Arithmetic operators/HelloWorld.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+
+int main() {
+    // Arithmetic operators = return the result of a specific arithmetic operation (+, -, *, /, %, ++, --)
+    // Parenthesis, multiplication & division, addition & subtraction
+
+
+    double students = 6 - (5 + 4) * 3 / 2;
+
+    // students = students + 1;
+    // students+=1;
+    // students++;
+
+    // students = students - 1;
+    // students-=1;
+    // students--;
+
+    // students = students * 2;
+    // students*=2;
+
+    // students = students / 2;
+    // students/=3;
+
+    std::cout << students << std::endl;
+
+    return 0;
+}
