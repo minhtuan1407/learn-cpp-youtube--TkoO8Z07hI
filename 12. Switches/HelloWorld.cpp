3fddb67416dc374bba7551d6cbfdc20a4bba4358
minhtuan1407 <m.tuan9a2@gmail.com>
2023-11-21 09:54:46 +0700

    change: update code session 12

---
+++ 12. Switches/HelloWorld.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+int main() {
+    
+    // switch = alternative to using many "else if" statements
+    // compare one value against matching cases
+
+    int month;
+    std::cout << "Enter a month number (1-12): ";
+    std::cin >> month;
+
+    switch(month){
+        case 1:
+            std::cout << "January";
+            break;
+        case 2:
+            std::cout << "February";
+            break;
+        case 3:
+            std::cout << "March";
+            break;
+        case 4:
+            std::cout << "April";
+            break;
+        case 5:
+            std::cout << "May";
+            break;
+        case 6:
+            std::cout << "June";
+            break;
+        case 7:
+            std::cout << "July";
+            break;
+        case 8:
+            std::cout << "August";
+            break;
+        case 9:
+            std::cout << "September";
+            break;
+        case 10:
+            std::cout << "October";
+            break;
+        case 11:
+            std::cout << "November";
+            break;
+        case 12:
+            std::cout << "December";
+            break;
+        default:
+            std::cout << "Not a month";
+            break;
+    }
+
+    return 0;
+}
