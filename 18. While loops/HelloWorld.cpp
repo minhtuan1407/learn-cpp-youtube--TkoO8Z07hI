8bf3a66fc58770260a8648bd73e43cbf6cae2553
minhtuan1407 <m.tuan9a2@gmail.com>
2023-11-22 11:06:55 +0700

    change: update code session 18

---
+++ 18. While loops/HelloWorld.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() {
+
+    std::string name;
+
+    while(name.empty()) {
+        std::cout << "Enter your name: ";
+        std::getline(std::cin, name);
+    }
+
+    std::cout << "Welcome " << name << std::endl;
+
+    return 0;
+}
